132eabcbcc355a2ea2164f4fc11eee41a25fc687
shaocq <shaocq@lenovo.com>
2022-04-22 11:19:59 +0800

    refactor code to readability

---
+++ ebookmanager/ebookmanager.go
@@ -0,0 +1,197 @@
+package ebookmanager
+
+import (
+	"crypto/sha256"
+	"database/sql"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type EBookItem struct {
+	Name string
+	Hash string
+}
+
+type EBookManager struct {
+	db    *sql.DB
+	items []EBookItem
+
+	rootPath string
+	dbPath   string
+}
+
+func NewEBookManager(rootPath string, dbPath string) *EBookManager {
+	m := EBookManager{
+
+		items:    make([]EBookItem, 0),
+		rootPath: rootPath,
+		dbPath:   dbPath,
+	}
+
+	return &m
+}
+
+func (m *EBookManager) Start() {
+	m.openDB()
+	m.createDBIfNotExist()
+	m.readDB()
+}
+
+func (m *EBookManager) RemoveDuplicated() {
+	m.processRoot()
+}
+
+func (m *EBookManager) Done() {
+	m.db.Close()
+}
+
+func (m *EBookManager) processRoot() {
+	m.processDirs(m.rootPath)
+}
+
+func (m *EBookManager) processDirs(path string) {
+	fileInfos, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Printf("ReadDir failed,error:%v\n", err)
+		return
+	}
+	for _, info := range fileInfos {
+		if info.IsDir() {
+			m.processDirs(path + "/" + info.Name())
+		} else {
+			m.processFile(info, path)
+		}
+	}
+}
+
+func (m *EBookManager) processFile(info fs.FileInfo, path string) {
+	fullpath := path + "/" + info.Name()
+	hash, err := calculateHash(fullpath)
+	if err == nil {
+		path, ok := m.findIt(hash)
+		if ok {
+			if path != fullpath {
+				removeFile(path, fullpath)
+			}
+		} else {
+			item := EBookItem{
+				Name: fullpath,
+				Hash: hash,
+			}
+			m.addToItems(item)
+			m.addToDB(item)
+		}
+	} else {
+		fmt.Printf("calculateHash failed,error:%v\n", err)
+	}
+}
+
+func (m *EBookManager) addToItems(item EBookItem) {
+	m.items = append(m.items, item)
+}
+
+func (m *EBookManager) createDBIfNotExist() {
+	db_dql := `
+	  CREATE TABLE "ebookinfo" (
+		'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+		'path' VARCHAR(1024) NOT NULL,
+		'hash' VARCHAR(128) NOT NULL
+	);
+	`
+
+	m.db.Exec(db_dql)
+}
+
+func (m *EBookManager) openDB() {
+	var err error
+	m.db, err = sql.Open("sqlite3", m.dbPath)
+	if err != nil {
+		fmt.Printf("Open db failed,error:%v\n", err)
+		return
+	}
+}
+
+func (m *EBookManager) readDB() {
+	rows, err := m.db.Query("SELECT * FROM ebookinfo")
+	if err != nil {
+		return
+	}
+
+	for rows.Next() {
+		var id string
+		var path string
+		var hash string
+		err = rows.Scan(&id, &path, &hash)
+		if err != nil {
+			break
+		}
+		item := EBookItem{
+			Name: path,
+			Hash: hash,
+		}
+		m.addToItems(item)
+	}
+}
+
+func (m *EBookManager) addToDB(item EBookItem) {
+	stmt, err := m.db.Prepare("INSERT INTO ebookinfo(path, hash) values(?,?)")
+	if err != nil {
+		return
+	}
+	_, err = stmt.Exec(item.Name, item.Hash)
+	if err != nil {
+		return
+	}
+}
+
+func (m *EBookManager) findIt(hash string) (string, bool) {
+	for _, item := range m.items {
+		if item.Hash == hash {
+			return item.Name, true
+		}
+	}
+	return "", false
+}
+
+func calculateHash(path string) (hash string, err error) {
+	fp, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	defer fp.Close()
+	buff := make([]byte, 8*1024)
+	m := sha256.New()
+	for {
+		lens, err := fp.Read(buff)
+		if err == io.EOF || lens < 0 {
+			break
+		}
+		m.Write(buff)
+	}
+	res := hex.EncodeToString(m.Sum(nil))
+	return res, nil
+}
+
+func removeFile(path string, fullpath string) {
+	//fmt.Printf("Find duplicated:%s with %s, hash:%s\n", fullpath, path, hash)
+	var tempName string
+	if strings.Contains(path, "(1)") || strings.Contains(path, "副本") {
+		tempName = path
+	} else {
+		tempName = fullpath
+	}
+	err := os.Remove(tempName)
+	if err != nil {
+		fmt.Printf("remove:%s failed\n", tempName)
+	} else {
+		fmt.Printf("remove:%s successfully\n", tempName)
+	}
+}
--- main.go
+++ main.go
@@ -1,152 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"database/sql"
-	"encoding/hex"
 	"flag"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"os"
-	"strings"
 
+	"github.com/ebookmanagement/ebookmanager"
 	_ "github.com/mattn/go-sqlite3"
 )
-
-type EbookItem struct {
-	Name string
-	Hash string
-}
-
-func listRoot(path string) {
-	listAllFiles(path)
-}
-
-func listAllFiles(path string) {
-	fileInfos, err := ioutil.ReadDir(path)
-	if err != nil {
-		fmt.Printf("ReadDir failed,error:%v\n", err)
-		return
-	}
-	for _, info := range fileInfos {
-		if info.IsDir() {
-			listAllFiles(path + "/" + info.Name())
-		} else {
-			fullpath := path + "/" + info.Name()
-			hash, err := calculateHash(fullpath)
-			if err == nil {
-				path, ok := findIt(hash)
-				if ok {
-					if path != fullpath {
-						//fmt.Printf("Find duplicated:%s with %s, hash:%s\n", fullpath, path, hash)
-						var tempName string
-						if strings.Contains(path, "(1)") || strings.Contains(path, "副本") {
-							tempName = path
-						} else {
-							tempName = fullpath
-						}
-						err = os.Remove(tempName)
-						if err != nil {
-							fmt.Printf("remove:%s failed\n", tempName)
-						} else {
-							fmt.Printf("remove:%s successfully\n", tempName)
-						}
-					}
-					//remove it
-				} else {
-					item := EbookItem{
-						Name: fullpath,
-						Hash: hash,
-					}
-					items = append(items, item)
-					addToDB(item)
-				}
-			} else {
-				fmt.Printf("calculateHash failed,error:%v\n", err)
-			}
-		}
-	}
-}
-
-func calculateHash(path string) (hash string, err error) {
-	fp, err := os.Open(path)
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-	defer fp.Close()
-	buff := make([]byte, 8*1024)
-	m := sha256.New()
-	for {
-		lens, err := fp.Read(buff)
-		if err == io.EOF || lens < 0 {
-			break
-		}
-		m.Write(buff)
-	}
-	res := hex.EncodeToString(m.Sum(nil))
-	return res, nil
-}
-
-func findIt(hash string) (string, bool) {
-	for _, item := range items {
-		if item.Hash == hash {
-			return item.Name, true
-		}
-	}
-	return "", false
-}
-
-func addToDB(item EbookItem) {
-	stmt, err := db.Prepare("INSERT INTO ebookinfo(path, hash) values(?,?)")
-	if err != nil {
-		return
-	}
-	_, err = stmt.Exec(item.Name, item.Hash)
-	if err != nil {
-		return
-	}
-}
-
-func readDB() {
-	//查询数据
-	rows, err := db.Query("SELECT * FROM ebookinfo")
-	if err != nil {
-		return
-	}
-
-	for rows.Next() {
-		var id string
-		var path string
-		var hash string
-		err = rows.Scan(&id, &path, &hash)
-		if err != nil {
-			break
-		}
-		item := EbookItem{
-			Name: path,
-			Hash: hash,
-		}
-		items = append(items, item)
-	}
-
-}
-
-var items = make([]EbookItem, 0)
-var db *sql.DB
-
-func createDBIfNotExist() {
-	db_dql := `
-	  CREATE TABLE "ebookinfo" (
-		'id' INTEGER PRIMARY KEY AUTOINCREMENT,
-		'path' VARCHAR(1024) NOT NULL,
-		'hash' VARCHAR(128) NOT NULL
-	);
-	`
-
-	db.Exec(db_dql)
-}
 
 func main() {
 	var rootPath string
@@ -154,13 +13,9 @@ func main() {
 	flag.StringVar(&rootPath, "path", ".", "root path")
 	flag.StringVar(&dbPath, "db", "./ebookinfo.db", "database path")
 	flag.Parse()
-	var err error
-	db, err = sql.Open("sqlite3", dbPath)
-	if err != nil {
-		fmt.Printf("Open db failed,error:%v\n", err)
-		return
-	}
-	createDBIfNotExist()
-	readDB()
-	listRoot(rootPath)
+
+	m := ebookmanager.NewEBookManager(rootPath, dbPath)
+	m.Start()
+	m.RemoveDuplicated()
+	m.Done()
 }
